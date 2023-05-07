3ed8376979216e5e5dc9f90fc6eef73e67a0dc22
Lloyd Allen <lloydallen597@gmail.com>
2023-05-06 22:08:46 -0500

    Added components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>Copyright © {new Date().getFullYear()}</footer>;
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Note() {
+  return (
+    <div className="note">
+      <h2>Note Title</h2>
+      <p>Note content</p>
+    </div>
+  );
+}
+
+export default Note;
